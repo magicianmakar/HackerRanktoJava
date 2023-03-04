2b3f45e35fc87bfb1aa17a9d0a71ab2fdce0d3d4
magicianmakar <magicianmakar1206@gmail.com>
2023-03-04 15:18:27 +0900

    HackerRank

--- src/HackerRank/JavaSort.java
+++ src/HackerRank/JavaSort.java
@@ -2,44 +2,48 @@ package HackerRank;
 
 import java.util.*;
 
-class Student{
+class Student {
 	private double cgpa;
 	private String fname;
 	private int id;
-	
-	public Student(int id , String fname , double cgpa) {
+
+	public Student(int id, String fname, double cgpa) {
 		super();
 		this.id = id;
-		this.fname =fname;
+		this.fname = fname;
 		this.cgpa = cgpa;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public String getString() {
 		return fname;
 	}
+
 	public double getCgpa() {
 		return cgpa;
 	}
 }
-
 
 public class JavaSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner read = new Scanner(System.in);
-		int n = read.nextInt();read.nextLine();
-		List<Student> studentList = new Student(id , fname , cgpa);
-		while(n-->0) {
+		int n = read.nextInt();
+		read.nextLine();
+		List<Student> studentList = new Student(id, fname, cgpa);
+		while (n-- > 0) {
 			int id = read.nextInt();
 			String fname = read.next();
 			double cgpa = read.nextDouble();
-			
+
 			Student st = new Student(id, fname, cgpa);
 			studentList.add(st);
 		}
+		read.close();
 	}
 
 }
